e59bc1d7f8651965feef5aee05768864f53d806e
MaximumFX <draconiconraveleijn@gmail.com>
2022-01-15 23:51:41 +0100

    :construction: Update NBT Editor route

--- src/js/path.js
+++ src/js/path.js
@@ -4,7 +4,7 @@ export const getRouteForFile = file => {
 	let route = ''
 	const ext = path.extname(file)
 	if (['.schem', '.nbt'].includes(ext)) {
-		route = '/nbt-explorer/editor'
+		route = '/nbt-editor/editor'
 	}
 	return route
 }
